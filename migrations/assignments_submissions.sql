de377af9b285b456827f244c89ecbfa98de5ea78
Dylan <dcren2010@gmail.com>
2021-04-26 16:11:35 -0400

    created assignment tables and import datas

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,20 @@
+DROP TABLE IF EXISTS assignments;
+
+CREATE TABLE assignments (
+id SERIAL PRIMARY KEY NOT NULL,
+name VARCHAR(225),
+content TEXT,
+day DATE,
+chapter INT,
+duration INT
+);
+
+DROP TABLE IF EXISTS assignment_submissions;
+
+CREATE TABLE assignment_submissions (
+id SERIAL PRIMARY KEY NOT NULL,
+assignment_id INT REFERENCES assignments(id) ON DELETE CASCADE,
+student_id INT REFERENCES students(id) ON DELETE CASCADE,
+duration INT,
+submission_date DATE
+);
